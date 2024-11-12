e838cb9512118188cba7cca8432dd08eed4abf58
DiogoSequeira1234 <48059@alunos.upt.pt>
2024-11-12 15:43:12 +0000

    inicio

---
+++ src/main/java/org/example/Admin.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Admin {
+    private int id;
+    private String nome;
+    private String epocaExames;// o administrador deve definir epocas de exames
+
+
+}
---
+++ src/main/java/org/example/Utilizador.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Utilizador {
+    private int id;
+    private String curso; // o utilizador deve ser capaz de selecionar o curso correspondente para a criacao do mapa
+    private String avaliacoes; // o utilizador deve preecher as avaliacoes das diferentes UC ao longo do semestre
+
+
+
+}
