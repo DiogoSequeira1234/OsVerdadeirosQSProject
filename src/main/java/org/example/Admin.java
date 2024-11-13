cd191866cbb806dbfb224a5314b40be669edf0df
CarlosJFCPereira <tkipar99@gmail.com>
2024-11-13 14:33:47 +0000

    Update Admin.java
    update no admin.java

--- src/main/java/org/example/Admin.java
+++ src/main/java/org/example/Admin.java
@@ -5,6 +5,31 @@ public class Admin {
     private String nome;
     private String epocaExames;// o administrador deve definir epocas de exames
 
+    public  Admin(int id, String nome, String epocaExames) {
+        this.id = id;
+        this.nome = nome;
+        this.epocaExames = epocaExames;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEpocaExames() {
+        return epocaExames;
+    }
+
+    public void setEpocaExames(String epocaExames) {
+        this.epocaExames = epocaExames;
+    }
+
+    public int getId() {
+        return id;
+    }
 
     //MÉTODOS a fazer
     //private void pridefinirSemestre(inicio: Date, fim: Date){
